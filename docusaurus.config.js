4f820690b3cff65c6ddc1a89d91b8bc03a64cee3
rmylb <r.lebrun@cen-na.org>
2022-09-16 12:58:17 +0200

    test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,7 +86,7 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
+                label: 'Test',
                 to: '/docs/intro',
               },
             ],
