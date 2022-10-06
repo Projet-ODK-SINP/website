473b17c8e2d7981e48126cbe4cd093f1931e327c
mathieubossaert <mathieu.bossaert@cenlr.org>
2022-10-06 12:24:17 +0200

    modif title site

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Formulaire ODK-SINP',
+  title: 'Formulaire ODK pour la conservation de la nature',
   tagline: 'Base documentaire',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/website/',
