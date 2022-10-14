e0ad01547d1a5a3a082bfca36b22a4dff503cd66
mathieubossaert <mathieu.bossaert@cenlr.org>
2022-10-14 09:59:38 +0200

    test modif accueil

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            &#128241; Aller aux formulaires &#128241;
           </Link>
         </div>
       </div>
@@ -30,8 +30,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Formulaires ODK créés et utilisés par les gestionnaires d'espaces naturels pour mener à bien leurs missiosns">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
