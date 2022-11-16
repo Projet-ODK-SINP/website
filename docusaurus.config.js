548cc2f56e74ed73be68d068d035ea5817f5b96d
rmylb <r.lebrun@cen-na.org>
2022-11-16 15:19:57 +0100

    hide assets folder

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,6 +38,8 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
+          // Assets folder
+          exclude: ['**/ODK-CEN/fichiers/**'],
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl: 
